1bbf1d84007a204588a4de0a9cbe041e4bd66ecb
daemonleaf <59505963+daemonleaf@users.noreply.github.com>
2020-10-16 18:09:05 -0300

    Initial Commit

---
+++ QuickNote/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  QuickNote
+//
+//  Created by Diogo Silva on 10/16/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
